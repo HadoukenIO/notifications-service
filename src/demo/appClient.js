f52472ee7a119e9c72660656b3ad0846dfd98c0e
Michael Coates <MichaelMCoates@gmail.com>
2018-05-16 13:35:13 -0400

    Restyle demo client app and add Brians response logger

--- src/demo/appClient.js
+++ src/demo/appClient.js
@@ -12,7 +12,7 @@ function getNotes() {
     return ofnotes.getAll();
 }
 
-let noteOptions1 = { 
+let normalNote = { 
     body: 'Notification Body', 
     title: 'Notification Title ', 
     subtitle: 'testSubtitle', 
@@ -21,7 +21,7 @@ let noteOptions1 = {
     date: Date.now()
 };
 
-let noteOptions2 = { 
+let buttonNote = { 
     body: 'Notification Body', 
     title: 'Notification Title ', 
     subtitle: 'testSubtitle', 
@@ -36,56 +36,94 @@ let noteOptions2 = {
 
 function makeNoteOfType(index) {
     if (index % 2 === 1) {
-        return makeNote(`1q2w3e4r${index}`, noteOptions1)
+        return makeNote(`1q2w3e4r${index}`, normalNote)
     } else {
-        return makeNote(`1q2w3e4r${index}`, noteOptions2)
+        return makeNote(`1q2w3e4r${index}`, buttonNote)
     }
 }
 
-document.addEventListener("DOMContentLoaded", function (event) {
+// document.addEventListener("DOMContentLoaded", function (event) {
+//     for (let index = 1; index < 7; index++) {
+//         document.getElementById(`button${index}`).addEventListener('click', () => {
+//             makeNoteOfType(index)
+//                 .then((notification) => {
+//                     if (!notification.success) {
+//                         // document.getElementById('clientResponse').innerHTML = `
+//                         //             Notification ids must be unique! This id already exists!
+//                         //         `
+//                         logit(`Notification ids must be unique! This id already exists!`);
+//                     }
+//                 })
+//         });
+    
+//         document.getElementById(`clearbutton${index}`).addEventListener('click', () => {
+//             clearNote(`1q2w3e4r${index}`)
+//         });
+//     }
+    
+//     document.getElementById(`fetchAppNotifications`).addEventListener('click', () => {
+//         getNotes()
+//             .then((notifications) => {
+//                 // document.getElementById('clientResponse').innerHTML = `
+//                 //     ${notifications.value.length} notifications received from the Notification Center!                
+//                 // `
+//                 logit(`${notifications.value.length} notifications received from the Notification Center`);
+//             })
+//     });
+// });
+
+fin.desktop.main(async () => {
+    const clientResponse = document.getElementById('clientResponse')
+
+
+    function logit(msg) {
+        const logEntry = document.createElement('div');
+        logEntry.innerHTML = msg;
+        clientResponse.insertBefore(logEntry, clientResponse.firstChild);
+    }
+
     for (let index = 1; index < 7; index++) {
         document.getElementById(`button${index}`).addEventListener('click', () => {
             makeNoteOfType(index)
                 .then((notification) => {
                     if (!notification.success) {
-                        document.getElementById('clientResponse').innerHTML = `
-                                    Notification ids must be unique! This id already exists!
-                                `
+                        // document.getElementById('clientResponse').innerHTML = `
+                        //             Notification ids must be unique! This id already exists!
+                        //         `
+                        logit(`Notification ids must be unique! This id already exists!`);
                     }
                 })
         });
-    
+
         document.getElementById(`clearbutton${index}`).addEventListener('click', () => {
             clearNote(`1q2w3e4r${index}`)
         });
     }
-    
+
     document.getElementById(`fetchAppNotifications`).addEventListener('click', () => {
         getNotes()
             .then((notifications) => {
-                document.getElementById('clientResponse').innerHTML = `
-                    ${notifications.value.length} notifications received from the Notification Center!                
-                `
+                // document.getElementById('clientResponse').innerHTML = `
+                //     ${notifications.value.length} notifications received from the Notification Center!                
+                // `
+                logit(`${notifications.value.length} notifications received from the Notification Center`);
             })
     });
-});
 
-
-
-
-
-fin.desktop.main(async () => {
     ofnotes.addEventListener('click', (payload) => {
-        document.getElementById('clientResponse').innerHTML = `CLICK action received from notification ${payload.id}`
+        // document.getElementById('clientResponse').innerHTML = `CLICK action received from notification ${payload.id}`
+        logit(`CLICK action received from notification ${payload.id}`);
     });
     
     ofnotes.addEventListener('close', (payload) => {
-        document.getElementById('clientResponse').innerHTML = `CLOSE action received from notification ${payload.id}`
+        // document.getElementById('clientResponse').innerHTML = `CLOSE action received from notification ${payload.id}`
+        logit(`CLOSE action received from notification ${payload.id}`);
     });
     ofnotes.addEventListener('button-click', (payload) => {
         let buttonClicked = payload.buttons[payload.buttonIndex]
         console.log(buttonClicked);
         let buttonTitle = buttonClicked.title
-        document.getElementById('clientResponse').innerHTML = `Button Click on ${buttonTitle} action received from notification ${payload.id}`
+        // document.getElementById('clientResponse').innerHTML = `Button Click on ${buttonTitle} action received from notification ${payload.id}`
+        logit(`BUTTON CLICK on ${buttonTitle} from notification ${payload.id}`);
     });
 });
