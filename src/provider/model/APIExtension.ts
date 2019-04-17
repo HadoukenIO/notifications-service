51d4d6f64ba39a9d300174e69b347739a8934325
Tom Roberts <troberts@openfin.co>
2019-04-17 11:05:04 +0100

    comment improvemnt

--- src/provider/model/APIExtension.ts
+++ src/provider/model/APIExtension.ts
@@ -1,9 +1,15 @@
+/**
+ * This file defines the hidden extensions to the api that the provider uses
+ * to communicate with the notifications center, and vice-versa
+ *
+ * They are included here instead of in client/internal.ts to keep them out
+ * of any published client code.
+ */
+
 import {API} from '../../client/internal';
 
 import {StoredNotification} from './StoredNotification';
 
-// These define the hidden extensions to the api
-// that are used for comms with the center
 export enum APITopicExtension {
     NOTIFICATION_CLICKED = 'notification-clicked',
     NOTIFICATION_BUTTON_CLICKED = 'notification-button-clicked',
