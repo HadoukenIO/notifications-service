8c708de8c70802c65a0a6cfb3ea04ad9a12afee4
Tom Roberts <troberts@openfin.co>
2019-05-28 13:22:18 +0100

    added retry mechanism to int test global setup

--- test/demo/utils/globalSetup.ts
+++ test/demo/utils/globalSetup.ts
@@ -22,7 +22,12 @@ module.exports = async () => {
     const devToolsPort = Number.parseInt(devToolsPortArgs[0].split('=')[1]);
 
     // Get the browser process devtools endpoint and connect to it with puppeteer to check it works
-    const browserWSEndpoint = (await (await fetch(`http://localhost:${devToolsPort}/json/version`)).json()).webSocketDebuggerUrl;
+    const fetchResponse = await fetch(`http://localhost:${devToolsPort}/json/version`).catch(async () => {
+        console.log('Initial fetch failed. Trying again in 1 second.');
+        await new Promise(res => setTimeout(res, 100000));
+        return fetch(`http://localhost:${devToolsPort}/json/version`);
+    });
+    const browserWSEndpoint = (await fetchResponse.json()).webSocketDebuggerUrl;
     if (!browserWSEndpoint) {
         throw new Error('Could not get webSocket endpoint for puppeteer connection');
     }
