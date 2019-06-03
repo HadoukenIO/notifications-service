edf0f08409018be64b630f359338c9efc62d8df6
Tom Roberts <troberts@openfin.co>
2019-06-03 13:52:39 +0100

    minor tweak to uuidv4 function

--- test/demo/utils/ofPuppeteer.ts
+++ test/demo/utils/ofPuppeteer.ts
@@ -137,7 +137,5 @@ function getIdString(identity: Identity): string {
 
 type AnyFunction = (...args: any[]) => any;
 function uuidv4() {
-    return (Math.random()
-        .toString(36)
-        .substring(2) + new Date().getTime().toString(36));
+    return (Math.random().toString(36).substring(2) + Date.now().toString(36));
 }
