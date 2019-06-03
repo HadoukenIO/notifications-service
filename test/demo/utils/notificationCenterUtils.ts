b910849dfd5c61f3678a9de208a6b2b653c42ad2
Tom Roberts <troberts@openfin.co>
2019-06-03 10:49:14 +0100

    fix incorrect class names in centerUtils

--- test/demo/utils/notificationCenterUtils.ts
+++ test/demo/utils/notificationCenterUtils.ts
@@ -8,15 +8,15 @@ const CENTER_IDENTITY = {uuid: 'notifications-service', name: 'Notification-Cent
 const ofBrowser = new OFPuppeteerBrowser();
 export async function getAllCards() {
     const centerPage = await ofBrowser.getPage(CENTER_IDENTITY);
-    return centerPage!.$$('.notification-item');
+    return centerPage!.$$('.notification');
 }
 export async function getCardsByApp(sourceUuid: string): Promise<ElementHandle[]> {
     const centerPage = await ofBrowser.getPage(CENTER_IDENTITY);
-    return centerPage!.$$(`.notification-item[data-id*="${sourceUuid}"]`);
+    return centerPage!.$$(`.notification[data-id*="${sourceUuid}"]`);
 }
 export async function getCardsByNotification(sourceUuid: string, notificationId: string): Promise<ElementHandle[]> {
     const centerPage = await ofBrowser.getPage(CENTER_IDENTITY);
-    return centerPage!.$$(`.notification-item[data-id="${sourceUuid}:${notificationId}"]`);
+    return centerPage!.$$(`.notification[data-id="${sourceUuid}:${notificationId}"]`);
 }
 
 export async function isCenterShowing(): Promise<boolean> {
