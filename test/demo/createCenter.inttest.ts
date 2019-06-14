6e1c5839a442bf12d328f044b6a9bd01146b0919
Tom Roberts <troberts@openfin.co>
2019-06-14 14:07:54 +0100

    change moment to default import in test

--- test/demo/createCenter.inttest.ts
+++ test/demo/createCenter.inttest.ts
@@ -3,7 +3,7 @@
 import 'jest';
 
 import {Application, Window} from 'hadouken-js-adapter';
-import * as moment from 'moment';
+import moment from 'moment';
 
 import {Notification, NotificationOptions} from '../../src/client';
 
