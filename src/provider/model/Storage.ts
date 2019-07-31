f48145a58183f64bd98a3fb7e27a94a5b5648e2e
ryanpetersonOF <ryan@openfin.co>
2019-07-31 12:06:24 +0100

    [SERVICE-571] Basic Upgrades

--- src/provider/model/Storage.ts
+++ src/provider/model/Storage.ts
@@ -25,3 +25,31 @@ export const notificationStorage = localforage.createInstance({
     storeName: 'notifications',
     version: PACKAGE_VERSION_NUM
 });
+
+settingsStorage.ready().then(async (): Promise<void> => {
+    const storedVersion = await settingsStorage.getItem<number>('dbVersion');
+    const dbVersion = settingsStorage.config().version!;
+
+    if (storedVersion < dbVersion) {
+        await upgradeDatabase();
+    }
+});
+
+async function upgradeDatabase(): Promise<void> {
+    const storedVersion = await settingsStorage.getItem<number>('dbVersion');
+    const dbVersion = settingsStorage.config().version;
+
+    // Example code.  Fill in actual upgrade paths here.
+    if (storedVersion < 2) {
+        // Do some upgrade to v2
+        await settingsStorage.setItem('dbVersion', 2);
+    } else if (storedVersion < 3) {
+        // Do some upgrade to v3
+        await settingsStorage.setItem('dbVersion', 3);
+    } else {
+        await settingsStorage.setItem('dbVersion', dbVersion);
+        return;
+    }
+
+    return upgradeDatabase();
+}
