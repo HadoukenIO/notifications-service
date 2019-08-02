45bee30f07f6391310690231cab217a52948dcf1
Noyan Gunday <noyan.gunday@openfin.co>
2019-08-02 10:01:58 +0100

    [SERVICE-572] Notifications Offline-Mode. Dynamic cache

--- src/provider/ServiceWorker.js
+++ src/provider/ServiceWorker.js
@@ -1,38 +1,5 @@
-// Set a name for the current cache
+// Set a name for 'he current cache
 const cacheName = PACKAGE_VERSION; // eslint-disable-line no-undef
-
-// Default files to always cache
-const cacheFiles = [
-    './provider.html',
-    './main-bundle.js',
-    './ui/toast-bundle.js',
-    './ui/notification-center.html',
-    './ui/toast.html',
-    './ui/favicon.ico',
-    './ui/favicon.png',
-    './ui/css/base.css',
-    './ui/css/notification-center.css',
-    './ui/css/toast.css',
-    './ui/css/AkkuratPro/AkkBdIt_Pro_1.otf',
-    './ui/css/AkkuratPro/AkkBd_Pro_1.otf',
-    './ui/css/AkkuratPro/AkkIt_Pro_1.otf',
-    './ui/css/AkkuratPro/AkkLgIt_Pro_1.otf',
-    './ui/css/AkkuratPro/AkkLg_Pro_1.otf',
-    './ui/css/AkkuratPro/AkkRg_Pro_1.otf',
-    './ui/css/themes/dark.css',
-    './ui/css/themes/light.css',
-    './ui/image/shapes/arrow.png',
-    './ui/image/shapes/notifications-x.png',
-    './ui/image/shapes/trayIcon.png'
-];
-
-self.addEventListener('install', (e) => {
-    // e.waitUntil Delays the event until the Promise is resolved
-    e.waitUntil(caches.open(cacheName).then((cache) => { // Open the cache
-        // Add all the default files to the cache
-        return cache.addAll(cacheFiles);
-    }));
-});
 
 self.addEventListener('activate', (e) => {
     e.waitUntil(caches.keys().then((cacheNames) => { // Get all the cache keys (cacheName)
@@ -46,8 +13,16 @@ self.addEventListener('activate', (e) => {
     }));
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request).catch(() => {
-        return caches.match(event.request);
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.match(event.request).then(response => {
+        if (response) {
+            return response;
+        }
+        return fetch(event.request).then(response => {
+            return caches.open(cacheName).then(cache => {
+                cache.put(event.request.url, response.clone());
+                return response;
+            });
+        });
     }));
 });
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,10 @@ const outputDir = path.resolve(__dirname, './dist');
 
 const webpackTools = require('openfin-service-tooling').webpackTools;
 
+node: {
+    fs: 'empty'
+}
+
 module.exports = [
     webpackTools.createConfig(`${outputDir}/client`, './src/client/index.ts', {minify: false, isLibrary: true, libraryName: 'notifications'}, webpackTools.versionPlugin),
     webpackTools.createConfig(`${outputDir}/client`, './src/client/index.ts', {minify: true, isLibrary: true, libraryName: 'notifications', outputFilename: 'openfin-notifications'}, webpackTools.versionPlugin),
