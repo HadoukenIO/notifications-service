ed82a6ea523e7b272702188870d7fe346ad526f0
Theo Ottah <48205674+TheoOttahOF@users.noreply.github.com>
2019-08-28 13:39:48 +0100

    Use reduceRight instead of sorting notifications
    Notifications are already in chronological order so reduceRight will reverse the list.

--- src/provider/view/utils/Grouping.ts
+++ src/provider/view/utils/Grouping.ts
@@ -66,12 +66,9 @@ export function getGroupTitle(notification: StoredNotification, groupingType: Gr
  * @param groupMethod Grouping type to use for sorting.
  */
 export function groupNotifications(notifications: StoredNotification[], groupMethod: GroupingType): Map<string, Group> {
-    // TODO: This sorting should be removed and changed to use insert-sort inside the reduce below
-    // Pre-sort notifications by date (groups will then also be sorted by date)
-    notifications.sort((a: StoredNotification, b: StoredNotification) =>
-        b.notification.date.valueOf() - a.notification.date.valueOf());
     // Reduce the notifications array into a Map of notifications grouped by their title
-    const groupMap = notifications.reduce((groups: Map<string, Group>, currentNotification: StoredNotification) => {
+    // Notifications are already in chronological order so we can just reduce from the right
+    const groupMap = notifications.reduceRight((groups: Map<string, Group>, currentNotification: StoredNotification) => {
         const groupTitle = getGroupTitle(currentNotification, groupMethod);
         // If group title already exists just add it to the group
         if (groups.has(groupTitle)) {
