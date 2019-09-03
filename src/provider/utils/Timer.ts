936bedf94bf0b5f79e71b51d4730a6fa77070dfa
bryangaleOF <bryan@openfin.co>
2019-09-03 13:44:23 +0100

    [SERVICE-588] Fix typing in Timer

--- src/provider/utils/Timer.ts
+++ src/provider/utils/Timer.ts
@@ -1,6 +1,6 @@
 export class Timer {
     private readonly _duration: number;
-    private _timeoutHandle: any | undefined;
+    private _timeoutHandle: number | undefined;
 
     constructor(duration: number) {
         this._duration = duration;
@@ -9,14 +9,14 @@ export class Timer {
     public start(): void {
         this.clear();
 
-        this._timeoutHandle = setTimeout(() => {
+        this._timeoutHandle = window.setTimeout(() => {
             this._timeoutHandle = undefined;
         }, this._duration);
     }
 
     public clear(): void {
         if (this._timeoutHandle !== undefined) {
-            clearTimeout(this._timeoutHandle);
+            window.clearTimeout(this._timeoutHandle);
         }
 
         this._timeoutHandle = undefined;
