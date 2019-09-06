b9885c19a450f30a51003043e4d1f15a6c41e950
ngunday <53172462+ngunday@users.noreply.github.com>
2019-09-06 10:07:31 +0100

    [Service Store] Replace redux store with service store implementation (#111)

--- test/demo/appRestart.inttest.ts
+++ test/demo/appRestart.inttest.ts
@@ -1,6 +1,6 @@
 import 'jest';
 
-import {Application, Window, Identity} from 'hadouken-js-adapter';
+import {Application, Window} from 'hadouken-js-adapter';
 import {ElementHandle} from 'puppeteer';
 
 import {NotificationOptions} from '../../src/client';
