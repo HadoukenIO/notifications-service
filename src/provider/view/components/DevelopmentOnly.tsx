891a6d7105bd2e1a9ff2b5d0e6d571bc0a510b06
bryangaleOF <bryan@openfin.co>
2019-09-06 14:14:54 +0100

    [SERVICE-588] Remove spurious logging

--- src/provider/view/components/DevelopmentOnly.tsx
+++ src/provider/view/components/DevelopmentOnly.tsx
@@ -6,7 +6,6 @@ export function DevelopmentOnly(props: DevelopementOnlyProps): React.ReactElemen
     if (process.env.NODE_ENV === 'production') {
         return null;
     } else {
-        console.log(props.children);
         return props.children ? props.children : null;
     }
 }
