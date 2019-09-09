f498fe866cfe91c6df3a578146a27d609e418a71
bryangaleOF <47325366+bryangaleOF@users.noreply.github.com>
2019-09-09 16:25:15 +0100

    [SERVICE-588] Close notification center on blur (#108)

--- src/provider/view/containers/NotificationCenterApp.tsx
+++ src/provider/view/containers/NotificationCenterApp.tsx
@@ -24,7 +24,7 @@ type Props = ReturnType<typeof mapStateToProps> & Actionable;
 
 export function NotificationCenterApp(props: Props) {
     const [groupBy, setGroupBy] = React.useState(GroupingType.DATE);
-    const {notifications, storeDispatch} = props;
+    const {notifications, storeDispatch, centerLocked} = props;
 
     return (
         <div className='notification-center'>
@@ -32,6 +32,7 @@ export function NotificationCenterApp(props: Props) {
                 groupBy={groupBy}
                 handleGroupBy={setGroupBy}
                 storeDispatch={storeDispatch}
+                centerLocked={centerLocked}
             />
             <NotificationView
                 notifications={notifications}
@@ -45,6 +46,7 @@ export function NotificationCenterApp(props: Props) {
 
 const mapStateToProps = (state: RootState, ownProps: Actionable) => ({
     notifications: state.notifications,
+    centerLocked: state.centerLocked,
     ...ownProps
 });
 
