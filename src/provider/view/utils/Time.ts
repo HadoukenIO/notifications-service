2d95ae6c9b4db204f2354b2cccb63dc1c201f30b
Theo Ottah <48205674+TheoOttahOF@users.noreply.github.com>
2019-09-16 14:48:01 +0100

    Fix timestamps for future dates

--- src/provider/view/utils/Time.ts
+++ src/provider/view/utils/Time.ts
@@ -5,19 +5,18 @@ const moment = require('moment');
 moment.updateLocale('en', {
     relativeTime: {
         future: 'in %s',
-        past: '%s',
-        s: 'just now',
-        ss: '%ss ago',
-        m: '1m ago',
-        mm: '%dm ago',
-        h: '1h ago',
+        past: '%s ago',
+        s: 'a moment',
+        m: '1m',
+        mm: '%dm',
+        h: '1h',
         hh: '%dh',
-        d: 'a day ago',
-        dd: '%dd',
-        M: 'a month ago',
-        MM: '%dM',
-        y: 'a year ago',
-        yy: '%dY'
+        d: 'a day',
+        dd: '%d days',
+        M: 'a month',
+        MM: '%d months',
+        y: 'a year',
+        yy: '%d years'
     }
 });
 
