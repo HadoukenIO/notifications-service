413558372b3a0dcc7140183c95a70d8ee3c0201f
Philip Broadbent <philip@openfin.co>
2020-02-10 14:32:19 +0000

    [N/A] Removing usused UI from settings panel

--- src/provider/view/components/SettingsView/SettingsView.tsx
+++ src/provider/view/components/SettingsView/SettingsView.tsx
@@ -6,17 +6,8 @@ import {ToggleCenterLocked, ToggleCenterMuted} from '../../../store/Actions';
 import {usePreduxStore} from '../../utils/usePreduxStore';
 import {ClassNameBuilder} from '../../utils/ClassNameBuilder';
 import {Toggle} from '../Controls/Toggle/Toggle';
-import {FeedSettings, Feed} from '../FeedSettings/FeedSettings';
-import {Button} from '../Controls/Button/Button';
 
 import * as Styles from './SettingsView.module.scss';
-
-const mockFeed: Feed = {
-    name: 'FTSE',
-    icon: 'https://cdn.pixabay.com/photo/2016/01/26/17/15/gmail-1162901_960_720.png'
-};
-
-const mockFeeds = new Array(5).fill(mockFeed);
 
 interface Props {
     centerLocked: boolean;
@@ -44,13 +35,6 @@ export const SettingsViewComponent: React.FC<Props> = (props) => {
                         }} />
                     </li>
                 </ul>
-            </div>
-            <div className={ClassNameBuilder.join(Styles, 'feed', 'section')}>
-                <h1>Notifications</h1>
-                <ul>
-                    {mockFeeds.map((f, i) => <li key={i}><FeedSettings {...f} /></li>)}
-                </ul>
-                <Button>Add a Feed</Button>
             </div>
         </div>
     );
